846d38de17182f2090df16f4f7ec73604ed68bbb
Alan Zhang <gagazcfan@icloud.com>
2020-07-27 20:16:43 +0800

    默认vuex模版created生命周期会被调用两次

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import './app.css'
 const App = new Vue({
   onShow (options) {
   },
+  created () {
+    console.log('这行会被打印两次')
+  },
   render(h) {
     // this.$slots.default 是将要会渲染的页面
     return h('block', this.$slots.default)
